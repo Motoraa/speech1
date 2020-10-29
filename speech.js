ce07ad2364b0fd9ddead57739e5ff8788c3c23f0
Motoraa <72560939+Motoraa@users.noreply.github.com>
2020-10-29 16:29:42 +0200

    Add files via upload

---
+++ speech.js
@@ -0,0 +1,27 @@
+document.addEventListener("touchstart",on_touch);
+document.addEventListener("mousedown",on_touch);
+var recognition = new webkitSpeechRecognition();
+recognition.lang = 'en-Us';
+function on_touch()
+{
+if(recognition.start){
+recognition.start();
+recognition_started = true;
+}
+}
+function onend()
+{
+recognition.stop();
+recognition_started = false;
+}
+recognition.onend = onend;
+recognition.onsounded = onend;
+recognition.onspeechend = onend;
+recognition.onresult =on_result;
+
+function on_result(e)
+{
+document.getElementById("text").innerHTML 
++="Ati rostit cuvantul: "+ e.result[0][0].transcript 
++ ", acuratete: "+e.results[0][0].confidence + "<br>"
+}
